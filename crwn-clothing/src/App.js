a272803860c6a82a7714a742c352a7df5018faee
hAlbers <halbers@mail.uni-paderborn.de>
2022-01-26 15:55:03 +0100

    add sign up with email and password

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shoppage/shoppage.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
     constructor() {
@@ -20,8 +20,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null
 
     componentDidMount() {
-        auth.onAuthStateChanged((user) => {
-            this.setState({ currentUser: user })
+        auth.onAuthStateChanged(async (userAuth) => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth)
+
+                userRef.onSnapshot((snapshot) => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data(),
+                        },
+                    })
+                })
+            }
+
+            this.setState({ currentUser: userAuth })
         })
     }
 
